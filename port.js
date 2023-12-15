9f86e483a6043f4c986d23009de9a479ef3fd822
Yvan Bissonnette <46501126+Bingobiss@users.noreply.github.com>
2023-12-15 04:23:00 +0000

    javascript added

--- port.js
+++ port.js
@@ -1,16 +1,28 @@
- // Sélectionnez toutes les images
- const images = document.querySelectorAll('.img-fluid');
+document.addEventListener("DOMContentLoaded", function(){
 
- // Parcourez chaque image et ajoutez un gestionnaire d'événement de clic
- images.forEach(image => {
-     image.addEventListener('click', () => {
-         // Changez la taille de l'image lorsqu'elle est cliquée
-         if (!image.classList.contains('enlarge')) {
-             // Ajoutez la classe 'enlarge' pour agrandir l'image
-             image.classList.add('enlarge');
-         } else {
-             // Supprimez la classe 'enlarge' pour revenir à la taille d'origine
-             image.classList.remove('enlarge');
-         }
-     });
- });
+    el_autohide = document.querySelector('.autohide');
+    
+    // add padding-top to bady (if necessary)
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+    document.body.style.paddingTop = navbar_height + 'px';
+  
+    if(el_autohide){
+      var last_scroll_top = 0;
+      window.addEventListener('scroll', function() {
+            let scroll_top = window.scrollY;
+           if(scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+      }); 
+      // window.addEventListener
+    }
+    // if
+  
+  }); 
+  // DOMContentLoaded  end
